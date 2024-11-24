c300e2d33fd3a1e54e13498372845ab4145cb562
Roni Seppälä <roni.o.seppala@gmail.com>
2024-11-24 22:53:59 +0200

    feature 1 added

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,63 @@
 import {Router, Request, Response} from "express"
+import fs from "fs"
 
 const router: Router = Router()
 
+type TUser = {
+    name: string,
+    todos: string[]
+}
+
 router.get("/hello", (req:Request, res:Response) => {
     res.json({"msg":"hello world"})
+})
+
+router.post("/add", (req:Request, res:Response) => {
+    const reqName = req.body.name
+    const reqTodo = req.body.todo
+
+    let todoData: TUser[] = [];
+
+    fs.readFile("data.json", "utf8", (error: NodeJS.ErrnoException | null, data: string) => {
+        if (error) {
+            console.error(error)
+            res.json({"msg":"read file error"})
+            return
+        }
+
+        try {
+            if (data != ""){
+                todoData = JSON.parse(data)
+            }
+        } catch (parseError: any) {
+            console.error(`Json parse error ${parseError}`)
+            res.json({"msg":"json parse error"})
+            return
+        }
+
+        const existingUser = todoData.find(user => user.name === reqName)
+
+        if (existingUser) {
+            existingUser.todos.push(reqTodo)
+        } else {
+            const newUser: TUser = {
+                name: reqName,
+                todos: [reqTodo]
+            }
+
+            todoData.push(newUser)
+        }
+
+
+        fs.writeFile("data.json", JSON.stringify(todoData), (error: NodeJS.ErrnoException | null) => {
+            if (error){
+                console.error(error)
+                res.json({"msg":"file write error"})
+                return
+            }
+            res.json({"msg":"gotted"})
+        })
+    })
 })
 
 export default router
